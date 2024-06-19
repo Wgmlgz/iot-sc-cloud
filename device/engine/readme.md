94b59fceffb355509ba29473ed8abfad7aaefd70
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-06-19 02:24:56 -0700

    Delete device directory

--- device/engine/readme.md
+++
@@ -1,2 +0,0 @@
-docker build -t device .
-docker run --device /dev/video0:/dev/video0 --name device
--- device/updates/package_update.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-if __name__ == '__main__':
-    subprocess.run(['docker', 'build', '-t', 'device-engine:latest', '.'], cwd='../device', check=True)
-    subprocess.run(['docker', 'save', '-o', '../package/device-engine.tar', 'device-engine:latest'], check=True)
-    subprocess.run(['tar', '-cvf', 'update_package.tar', '../package'], check=True)
-    os.remove('device-engine.tar')
