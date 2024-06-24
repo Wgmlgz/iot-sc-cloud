7f4a701a88fe89d73c8b47aae0e76747af17c112
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-06-24 17:25:38 +0300

    multiple widgets fix

--- thingsboard/tb-widget-core/vite.config.ts
+++ thingsboard/tb-widget-core/vite.config.ts
@@ -3,11 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
-import UnoCSS from 'unocss/vite'
+import UnoCSS from 'unocss/vite';
 
 // try use it its jwt  lol TODO move to .env
 const TOKEN =
-  'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiOGU5YWU0YjAtMTlkYy0xMWVmLThmZDYtNmJhNGFjMTdiM2YzIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiJiOTg0NmJhYi04ZWYyLTQ0ZjctYTI4ZS1hYzBmYjJmYmVjODciLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTcxOTE4MTI5MiwiZXhwIjoxNzE5MTkwMjkyLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOGRiYTZmYzAtMTlkYy0xMWVmLThmZDYtNmJhNGFjMTdiM2YzIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.01BwMuNGl5CLvifGIdp0mu2pTEgLP-Y-Ghgrvv-zFjlV_Pakqv4DrRaNQ3n3kDVEkUZPm120T2mrl4_7oCnH1A';
+  'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwidXNlcklkIjoiOGU5YWU0YjAtMTlkYy0xMWVmLThmZDYtNmJhNGFjMTdiM2YzIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJzZXNzaW9uSWQiOiI5ZTM0ZGM1Yy0wMDlhLTRiZTMtOGQ3Ny0yYWU1M2JmZGVkNDUiLCJpc3MiOiJ0aGluZ3Nib2FyZC5pbyIsImlhdCI6MTcxOTIzMjA0NywiZXhwIjoxNzE5MjQxMDQ3LCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiOGRiYTZmYzAtMTlkYy0xMWVmLThmZDYtNmJhNGFjMTdiM2YzIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCJ9.HQD_MT__85LYyn63og6FfyidVXd2n0XF2a8P4INzaPqBnMlHf4YEGQpQMWIxQ4BWjEYCdK2-D55494rpqpLRYg';
 
 const TB_URL = 'http://158.160.155.134:8080';
 //  '/api/widgetType/{widgetTypeId}{?inlineImages}'
@@ -29,14 +29,14 @@ const WidgetEntryPlugin = (options: WidgetEntryPluginOptions): Plugin => {
   async function uploadBuildFiles(options: WidgetEntryPluginOptions) {
     // Example function to handle file upload logic to ThingsBoard
     console.log('Uploading build files to ThingsBoard...');
-
+    console.log(out_paths);
     await Promise.all(
       out_paths.map(async ({ configFilePath, jsFilePath }) => {
         try {
           const js = fs.readFileSync(jsFilePath, 'utf8');
 
           const obj = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
-
+          console.log('Loading ', jsFilePath);
           const { widgetTypeId, extraDescriptor } = obj;
           const { data } = await axios.get(
             `${TB_URL}/api/widgetType/${widgetTypeId}`,
@@ -61,6 +61,7 @@ const WidgetEntryPlugin = (options: WidgetEntryPluginOptions): Plugin => {
               },
             }
           );
+          console.log('done', jsFilePath);
         } catch (e) {
           console.error(e);
         }
@@ -86,6 +87,11 @@ const WidgetEntryPlugin = (options: WidgetEntryPluginOptions): Plugin => {
       // Generate an entry file for each widget
       fs.readdirSync(path.resolve(config.root, widgetsDir))
         .filter(file => file.endsWith('.svelte'))
+        .filter(file =>
+          process.argv.some(arg => 
+            arg.includes(file.replace(/\.svelte$/, ''))
+          )
+        )
         .forEach(file => {
           const widgetName = file.replace(/\.svelte$/, '');
           const entryContent = template.replace(/__WIDGET__/g, widgetName);
@@ -119,35 +125,47 @@ const WidgetEntryPlugin = (options: WidgetEntryPluginOptions): Plugin => {
   };
 };
 
-export default defineConfig({
-  plugins: [
-    UnoCSS(),
-    svelte(),
-    WidgetEntryPlugin({
-      widgetsDir: 'src/widgets',
-      outputDir: 'src/entries',
-      initTemplatePath: 'src/initTemplate.js',
-    }),
-  ],
-  build: {
-    // minify: false,
-    rollupOptions: {
-      // Dynamically set input based on generated entry files
-      input: (() => {
-        const entriesDir = path.resolve(__dirname, 'src', 'entries');
-        return fs.readdirSync(entriesDir).reduce((entries: any, file) => {
-          if (file.endsWith('.ts')) {
-            const name = file.replace(/\.ts$/, '');
-            entries[name] = path.resolve(entriesDir, file);
-          }
-          return entries;
-        }, {});
-      })(),
-      output: {
-        format: 'iife', // Immediate Invoked Function Expression
-        dir: 'dist',
-        entryFileNames: `[name].js`,
+const input = (() => {
+  const entriesDir = path.resolve(__dirname, 'src', 'entries');
+  return fs.readdirSync(entriesDir).reduce((entries: any, file) => {
+    if (file.endsWith('.ts')) {
+      const name = file.replace(/\.ts$/, '');
+      process.argv.forEach(arg => {
+        if (arg.includes(name)) {
+          entries[name] = path.resolve(entriesDir, file);
+        }
+      });
+    }
+    return entries;
+  }, {});
+})();
+
+console.log(process.argv);
+
+export default defineConfig(t => {
+  console.log(t);
+  return {
+    plugins: [
+      UnoCSS(),
+      svelte(),
+      WidgetEntryPlugin({
+        widgetsDir: 'src/widgets',
+        outputDir: 'src/entries',
+        initTemplatePath: 'src/initTemplate.js',
+      }),
+    ],
+    build: {
+      // minify: false,
+      rollupOptions: {
+        // Dynamically set input based on generated entry files
+        input,
+        output: {
+          format: 'iife',
+          dir: 'dist',
+          entryFileNames: `[name].js`,
+          chunkFileNames: '[name].js',
+        },
       },
     },
-  },
+  };
 });
